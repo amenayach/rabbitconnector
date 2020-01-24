907b5083754bcc34aeb3bb877f7ce02ee63a6c90
Amen Ayach <aayach@Netways.com>
2020-01-24 11:22:52 +0200

    ic

---
+++ RabbitMqConnector/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RabbitMqConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the host(s) comma seperated:");
+                var hosts = Console.ReadLine().Split(',');
+
+                Console.WriteLine("Please enter the username:");
+                var username = Console.ReadLine();
+
+                Console.WriteLine("Please enter the password:");
+                var password = Console.ReadLine();
+
+                Console.WriteLine("Please enter the VirtualHost:");
+                var vhost = Console.ReadLine();
+
+                Console.WriteLine("Please enter the port:");
+                int.TryParse(Console.ReadLine(), out int port);
+
+                var rabbitService = new RabbitMqService(hosts, username, password, vhost, port);
+
+                rabbitService.Run();
+
+                Console.WriteLine("Hit enter to exit");
+                Console.ReadLine();
+
+                rabbitService.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMqConnector/RabbitMqService.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMqConnector
+{
+    public class RabbitMqService
+    {
+        private readonly IConnection connection;
+        private readonly IModel model;
+        private const string queueName = "TEST";
+        private const string exchangeName = "exname";
+
+        public RabbitMqService(IEnumerable<string> hosts, string username, string password, string vhost, int port)
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = username,
+                Password = password,
+                VirtualHost = vhost,
+                Port = port
+            };
+
+            connection = factory.CreateConnection(hosts.ToList());
+
+            model = connection.CreateModel();
+
+            model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            model.QueueDeclare(queueName, false, false, false, null);
+            model.QueueBind(queueName, exchangeName, queueName, null);
+        }
+
+        internal void Run()
+        {
+            //Listening to messages
+            var consumer = new EventingBasicConsumer(model);
+
+            Console.WriteLine("Listening to messages...");
+
+            consumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body;
+
+                Console.WriteLine(Encoding.UTF8.GetString(body));
+
+                model.BasicAck(ea.DeliveryTag, false);
+            };
+
+            model.BasicConsume(queueName, false, consumer);
+
+            //Sending test message
+            Console.WriteLine("Sending test message...");
+
+            model.BasicPublish(exchangeName, queueName, null, Encoding.UTF8.GetBytes("{\"Msg\": \"Message from RabbitMQ connector\"}"));
+        }
+
+        internal void Close()
+        {
+            model.Dispose();
+            connection.Dispose();
+        }
+    }
+}
